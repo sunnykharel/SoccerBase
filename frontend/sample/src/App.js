e1917421cc6533cdc76f58d75b150fc09c1abc81
askarpoudyal <askarpoudyal@utexas.edu>
2020-03-02 17:24:02 -0600

    reposting home and aboutus page

---
+++ frontend/sample/src/App.js
@@ -0,0 +1,32 @@
+import React, {Component} from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import {Link} from 'react-router-dom'
+import aboutUs from './aboutUs'
+import Home from './Home'
+
+import Navbar from './components/Navbar';
+import { render } from '@testing-library/react';
+import BackgroundCarousel from './backgroundCarousel'
+
+
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div id="App">
+          <Navbar />
+          <Switch>
+             <Route path="/" component={Home} exact/>
+             <Route path="/aboutUs" component={aboutUs}/>
+             
+            {/* <Route component={Error}/> */}
+           </Switch>
+        </div>
+        {/* <BackgroundCarousel /> */}
+      </BrowserRouter>
+      
+    );
+  }
+}
+
+export default App;
