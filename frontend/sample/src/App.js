d7917243887ca90fdd1ca7515d3dc0f7dda1118e
askarpoudyal <askarpoudyal@utexas.edu>
2020-03-02 21:24:25 -0600

    added 14 pages with 3 instances for each of the models

--- frontend/sample/src/App.js
+++ frontend/sample/src/App.js
@@ -3,6 +3,10 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import {Link} from 'react-router-dom'
 import aboutUs from './aboutUs'
 import Home from './Home'
+import Teams from './Teams'
+import Leagues from './Leagues'
+import Players from "./Players"
+
 
 import Navbar from './components/Navbar';
 import { render } from '@testing-library/react';
@@ -17,6 +21,9 @@ class App extends Component {
           <Navbar />
           <Switch>
              <Route path="/" component={Home} exact/>
+             <Route path ="/Teams" component={Teams}/>
+             <Route path ="/Leagues" component={Leagues}/>
+             <Route path ="/Players" component={Players}/>
              <Route path="/aboutUs" component={aboutUs}/>
              
             {/* <Route component={Error}/> */}
---
+++ frontend/sample/src/BPL.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+// import './aboutUs.css'
+
+
+const BPL = () => {
+    return (
+       <div style={{backgroundColor : "#BA55D3", paddingTop: "10px" , paddingBottom : "600px"}}>
+                 
+        <h1> BPL !!!!!!!</h1>
+         
+        
+       </div>
+        
+    );
+}
+ 
+export default BPL;
