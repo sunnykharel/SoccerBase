e7da51e21598ec6295746c53c3dd49c3e08e098d
parm <grewal.s.parm@gmail.com>
2020-03-06 17:46:47 -0600

    about us page has required phase 1 stats

--- frontend/sample/src/aboutUs.js
+++ frontend/sample/src/aboutUs.js
@@ -1,12 +1,40 @@
-import React from 'react';
 import Askar from './Askar.png';
 import Nithin from './Nithin.png';
 import Sunny from './Sunny.png';
+import React, { Component } from 'react'
 
 import './aboutUs.css'
 
+const httpCommits = new XMLHttpRequest();
+const urlCommits = "https://api.github.com/repos/sunnykharel/SoccerBase/stats/commit_activity";
+httpCommits.open("GET",urlCommits);
+httpCommits.setRequestHeader("Authorization", "Basic " + btoa("grewalparm:Verybadpassword1!"));
+httpCommits.send();
 
-const About = () => {
+httpCommits.onreadystatechange = (error) => {
+  console.log(httpCommits.responseText);
+}
+
+const httpIssues = new XMLHttpRequest();
+const urlIssues = "https://api.github.com/repos/sunnykharel/SoccerBase/issues";
+httpIssues.open("GET",urlIssues);
+httpIssues.setRequestHeader("Authorization", "Basic " + btoa("grewalparm:Verybadpassword1!"));
+httpIssues.send();
+
+httpIssues.onreadystatechange = (error) => {
+  console.log(httpIssues.responseText);
+  console.log(JSON.parse(httpIssues.response).length);
+}
+
+class About extends Component {
+  constructor(props) {
+    super(props);
+    var commits = JSON.parse(httpCommits.response)
+    this.state = {totalCommits: commits[commits.length - 1].total, 
+        totalIssues: JSON.parse(httpIssues.response).length, 
+        totalUnitTests: 0};
+  }
+  render() {
     return (
        <div style={{backgroundColor : "#5F9EA0", paddingTop : "10px"}}>
           <h1 style = {{textAlign: "center"}} >About Us</h1>
@@ -33,14 +61,21 @@ const About = () => {
             </div>
 
             <div>
-            <h1> Add GitHub Stuff here:</h1>
-
+                <h1> Add GitHub Stuff here:</h1>
+                <h5> Total Commits: {this.state.totalCommits} </h5>
+                <h5> Total Issues: {this.state.totalIssues} </h5>
+                <h5> Total Unit Tests: {this.state.totalUnitTests} </h5>
             </div>
 
           </div>
        </div>
      
     );
+  }
+
+  componentDidMount() {
+    
+  }
 }
  
 export default About;
