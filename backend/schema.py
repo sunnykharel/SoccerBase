a8feb218fd65ccbce855874abccf76731ae5f9ce
tbidnurkar <tbidnurkar@utexas.edu>
2020-03-27 16:47:48 -0500

    keep old schema

--- backend/schema.py
+++ backend/schema.py
@@ -14,20 +14,6 @@ import json
 
 
 
-<<<<<<< HEAD
-connect("countrydatabase", alias = 'country')
-connect("leaguedatabase", alias = 'league')
-connect("teamdatabase", alias = 'team')
-
-print("connected to the database")
-
-# class Country(Document):
-#     name = StringField(unique=True, required=True)
-#     code = StringField(required=True)
-#     flag = StringField(required=True)
-#     population = IntField()
-#     leaguecount = IntField()
-=======
 connect(db="countrydatabase", alias = 'country')
 connect(db="leaguedatabase", alias = 'default')
 connect(db="teamdatabase", alias = 'team')
@@ -43,47 +29,29 @@ class Country(Document):
     meta = {
         "db_alias":"country" 
     }
->>>>>>> d6fc0ace9c4f99f4610bb243b3bd4ad422d4673e
 
-#     meta = {
-#         "db_alias":"country" 
-#     }
+class League(Document):
+    league_id = IntField(required=True)
+    name = StringField(required=True)
+    type_ = StringField()
+    country = StringField(required=True)
+    country_code = StringField()
+    season = IntField(required=True)
+    season_start = StringField()
+    season_end = StringField()
+    logo = StringField()
+    flag = StringField()
+    coverage = DictField()
 
-# class League(Document):
-#     league_id = IntField(required=True)
-#     name = StringField(required=True)
-#     type_ = StringField()
-#     country = StringField(required=True)
-#     country_code = StringField()
-#     season = IntField(required=True)
-#     season_start = StringField()
-#     season_end = StringField()
-#     logo = StringField()
-#     flag = StringField()
-#     coverage = DictField()
+    def json(self):
+        league_dict = {
+            "name": self.name,
+            "logo": self.logo,
+            "country": self.country,
+            "season": self.season,
+        }
+        return json.dumps(league_dict)
 
-<<<<<<< HEAD
-#     def json(self):
-#         league_dict = {
-#             "name": self.name,
-#             "logo": self.logo,
-#             "country": self.country,
-#             "season": self.season,
-#         }
-#         return json.dumps(league_dict)
-
-#     meta = {
-#         "indexes": ["name", "country"],
-#         "ordering": ["name"],
-#         "db_alias":"league"
-#     }
-    
-# class Team(Document):
-#     name = StringField(required=True)
-#     meta = {
-#         "db_alias":"team"
-#     }
-=======
     meta = {
         "indexes": ["name", "country"],
         "ordering": ["name"],
@@ -99,4 +67,3 @@ class Team(Document):
     meta = {
         "db_alias":"team"
     }
->>>>>>> d6fc0ace9c4f99f4610bb243b3bd4ad422d4673e
