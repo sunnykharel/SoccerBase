a98987b6ca3bc83514ef1693a1269804fba3163a
grewalparm <grewal.s.parm@gmail.com>
2020-03-30 16:03:48 -0700

    added more about page content. Edited style

--- frontend/src/js/aboutUs.js
+++ frontend/src/js/aboutUs.js
@@ -15,8 +15,18 @@ class Card extends Component {
                 <li className="card">
                     <div className="cardContent">
                         <h3 className="cardTitle"> {this.props.name} </h3>
+                        <p className="bio">
+                            Bio <br>
+                            Area of Study: ECE Software/AE <br>
+                            Primary Responsibilities: frontend
+                        </p>
+                        <p className="gitStats">
+                            Total Commits:{this.props.commits}
+                            Total Issues:{this.props.issues}
+                            Total tests:{this.props.tests}
+                        </p>
                     </div>
-                    <img className="cardImage" src={Parm} />
+                    <img src={this.props.img} />
                 </li>
             </ul>
         );
@@ -28,9 +38,16 @@ class CardList extends Component {
     render() {
         return(
             <div className="cardDiv">
-                {this.props.names.map((name) => {
+                {this.props.users.map((user) => {
                     return (
-                        <Card name={name}  />
+                        <Card 
+                            name={user.name}
+                            ghUsername={user.ghUsername}
+                            img = {user.img}
+                            numCommits = {user.commits}
+                            numTests = {user.tests}
+                            numIssues = {user.issues}
+                        />
                     );
                 })}
             </div>
@@ -65,14 +82,130 @@ class About extends Component {
             nithinUnitTests: "loading unit tests..."
         }
     }
+    
+    componentDidMount() {
+        //Getting total commits
+        const scope = this;
+        var httpCommits = new XMLHttpRequest();
+        var urlCommits = "https://api.github.com/repos/sunnykharel/SoccerBase/stats/commit_activity";  
+        httpCommits.open("GET",urlCommits, true);
+        httpCommits.setRequestHeader("Authorization", "Basic " + btoa("tempforproj:Verybadpassword1!"));
+        httpCommits.onload = function(){
+          if (this.status == 200){
+            var commits = JSON.parse(httpCommits.responseText);
+            var totalCommits_ = 0;
+            for (let i = 0; i < commits.length; i++) {
+                totalCommits_ += commits[i].total;
+            }
+            scope.setState({
+              totalCommits: totalCommits_,
+            });
+          }
+          else {
+            console.warn("Let's assume this works");
+          }
+        }
+        httpCommits.send();
+
+        //Getting commits by contributor
+        
+
+        //Getting issues by contributor and total issues
+        var httpIssues = new XMLHttpRequest();
+        var urlIssues = "https://api.github.com/repos/sunnykharel/SoccerBase/issues";
+        httpIssues.open("GET",urlIssues, true);
+        httpIssues.setRequestHeader("Authorization", "Basic " + btoa("tempforproj:Verybadpassword1!"));
+        httpIssues.onload = function() {
+          console.log(httpIssues.responseText);
+          if (this.status == 200){
+            var issues = JSON.parse(httpIssues.responseText);
+            let parm = 0;
+            let sunny = 0;
+            let tanay = 0;
+            let nithin = 0;
+            let askar = 0;
+            for (let i = 0; i < issues.length; i++) {
+                issue = issues[i];
+                if ("grewalparm" = issue.user.login) {
+                    parm++;
+                } else if ("sunnykharel" == issue.user.login) {
+                    sunny++;
+                } else if ("tanay-bidnurkar" == issue.user.login) {
+                    tanay++;
+                } else if ("askarpoudyal" == issue.user.login) {
+                    askar++;
+                } else if ("nithinsaurus" == issue.user.login) {
+                    nithin++;
+                }
+            }
+            scope.setState({
+              totalIssues: issues.length,
+              parmIssues: parm,
+              askarIssues: askar,
+              nithinIssues: nithin,
+              sunnyIssues: sunny,
+              tanayIssues: tanay
+            });
+          }
+          else {
+            console.warn("Let's assume this works");
+          }
+        }
+        httpIssues.send();
+        
+        scope.setState({
+          totalUnitTests: 0,
+        });         
+    }
 
     render () {
+        //team member meta data
+        var users = [
+            {
+                name: "Parm",
+                ghUsername: "grewalparm",
+                img: Parm,
+                commits: this.state.parmCommits,
+                issues: this.state.parmIssues,
+                tests: this.state.parmUnitTests
+            },
+            {
+                name: "Tanay",
+                ghUsername: "tanay-bidnurkar",
+                img: Tanay,
+                commits: this.state.tanayCommits,
+                issues: this.state.tanayIssues,
+                tests: this.state.tanayUnitTests
+            },
+            {
+                name: "Askar",
+                ghUsername: "askarpoudyal",
+                img: Askar
+                commits: this.state.askarCommits,
+                issues: this.state.askarIssues,
+                tests: this.state.askarUnitTests
+            },
+            {
+                name: "Sunny",
+                ghUsername: "sunnykharel",
+                img: Sunny
+                commits: this.state.sunnyCommits,
+                issues: this.state.sunnyIssues,
+                tests: this.state.sunnyUnitTests
+            },
+            {
+                name: "Nithin",
+                ghUsername: "nithinsaurus",
+                img: Nithin
+                commits: this.state.nithinCommits,
+                issues: this.state.nithinIssues,
+                tests: this.state.nithinUnitTests
+            }
+        ];
+    
         return(
             <div className="clwrapper">
-                <CardList
-                    names={["Parm","Tanay", "Askar", "Sunny", "Nithin"]}
-                    ghUsernames={["grewalparm","askarpoudyal","tanay-bidnurkar","sunnykharel","nithinsaurus"]}
-                />
+                <CardList users={users}/>
             </div>
         );
     }
