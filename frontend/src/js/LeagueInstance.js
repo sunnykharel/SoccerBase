ced9ff13af4e30fbf29c63b9da942bda9bfcff40
askarpoudyal <askarpoudyal@utexas.edu>
2020-04-01 21:43:15 -0500

    finished the basic models page

--- frontend/src/js/LeagueInstance.js
+++ frontend/src/js/LeagueInstance.js
@@ -5,6 +5,14 @@ import React, { useState, useEffect ,Component} from 'react';
 import Posts from './components/Posts';
 import Pagination from './components/Pagination';
 import axios from 'axios';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 // import './App.css';
 
 class LeagueInstance extends Component {
@@ -41,10 +49,35 @@ class LeagueInstance extends Component {
                 break;
             }
         }
-         
-        //console.log(typeof this.state.i);
+        const useStyles = makeStyles({
+            root: {
+              maxWidth: 345,
+            },
+          });
         return (   
-            <h1>{this.state.responses_arrays[this.state.i].name}</h1>
+
+            //<h1>{this.state.i} </h1>
+            
+           // <h1>{this.state.responses_arrays[0].name}</h1>
+            <Card id="LeagueCard">
+         <CardActionArea>
+         <Typography gutterBottom variant="h5" component="h2">
+             League name: {this.state.responses_arrays[this.state.i].name}
+          </Typography>
+          
+        <Typography gutterBottom variant="h5" component="h2">
+             Country name: {this.state.responses_arrays[this.state.i].country}({this.state.responses_arrays[this.state.i].country_code})
+          </Typography>
+          <Typography gutterBottom variant="h5" component="h2">
+             Season start date: {this.state.responses_arrays[this.state.i].season_start}
+          </Typography>
+          <Typography gutterBottom variant="h5" component="h2">
+          League Logo:
+          </Typography>
+          <img src = {this.state.responses_arrays[this.state.i].logo} alt="no image found"/>
+      </CardActionArea>
+    </Card>
+            
         );
     }
 
